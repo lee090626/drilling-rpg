bc71bb5d2d7685bd868c13ddca7766360fb6e9ed
lee090626 <hyeongseogi46@gmail.com>
2026-02-14 05:36:50 +0900

    Updates

--- _next/static/LDxfha8nn3N-1Wx2zZiiI/_buildManifest.js
+++ _next/static/LDxfha8nn3N-1Wx2zZiiI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/drilling-rpg//_next/:path+",
+        "destination": "/drilling-rpg/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
